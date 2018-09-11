289b83e360eb825c9a7e59c8101f1d2484ed887a
Pascal <freak9191@web.de>
2018-09-11 17:23:10 +0200

    moved to Polymer 3, switched from bower to npm

---
+++ socket-io.js
@@ -0,0 +1,68 @@
+import {PolymerElement, html} from '@polymer/polymer';
+
+  window.__getSocket = (ep) => {
+    const endpoint = ep || '';
+
+    if (!window.__SOCKETS) {
+      window.__SOCKETS = {};
+    }
+
+    if (!window.__SOCKETS[endpoint]) {
+      window.__SOCKETS[endpoint] = io(endpoint);
+    }
+
+    return window.__SOCKETS[endpoint];
+  };
+
+  class SocketIO extends PolymerElement {
+    static get properties() {
+      return {
+        endpoint: {
+          type: String,
+          value: undefined
+        },
+        connected: {
+          type: Boolean,
+          readOnly: true,
+          notify: true,
+          value: false,
+          reflectToAttribute: true
+        },
+        events: {
+          type: Object,
+          value: {}
+        }
+      }
+    }
+
+    constructor() {
+      super();
+
+      this.socket = null;
+    }
+
+    connectedCallback() {
+      this.socket = window.__getSocket(this.endpoint);
+
+      this.socket.on('connected', () => this._onConnect());
+      this.socket.on('disconnected', () => this._onDisconnect());
+
+      Object.keys(this.events).forEach((key) => {
+        this.socket.on(key, this.events[key]);
+      })
+    }
+
+    emit(key, value) {
+      this.socket.emit(key, value);
+    }
+
+    _onConnect() {
+      this._setConnected(true);
+    }
+
+    _onDisconnect() {
+      this._setConnected(false);
+    }
+  }
+
+  window.customElements.define('socket-io', SocketIO);
